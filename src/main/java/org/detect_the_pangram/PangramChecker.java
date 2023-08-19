503427eeeda39c6f9fca80f29d5528b665bf3c89
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-08-19 08:33:31 +0100

    feat(pangram-checker): add first test to pangram checker

---
+++ src/main/java/org/detect_the_pangram/PangramChecker.java
@@ -0,0 +1,7 @@
+package org.detect_the_pangram;
+
+public class PangramChecker {
+    public boolean isPangram(String text) {
+        return false;
+    }
+}
---
+++ src/test/java/unit/detect_the_pangram/TestPangramDetectorShould.java
@@ -0,0 +1,20 @@
+package unit.detect_the_pangram;
+
+import org.detect_the_pangram.PangramChecker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.word_wrap.WordWrap;
+
+public class TestPangramDetectorShould {
+//    ! sentence.includes(alphabet) -> return false
+//    sentence.includes(alphabet) -> return true
+
+    @Test
+    public void detect_a_text_that_is_not_a_pangram() throws Exception {
+        String text = "This is not a pangram";
+
+        PangramChecker checker = new PangramChecker();
+
+        Assert.assertFalse(checker.isPangram(text));
+    }
+}
