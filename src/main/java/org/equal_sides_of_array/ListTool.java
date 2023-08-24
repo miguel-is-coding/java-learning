b57bc4dcd4b738c7c9952442bd281c04b68b1f84
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-08-24 19:02:40 +0100

    feat(equal-sides-of-array): add test case when numbers list does not have even index

--- src/main/java/org/equal_sides_of_array/ListTool.java
+++ src/main/java/org/equal_sides_of_array/ListTool.java
@@ -2,6 +2,13 @@ package org.equal_sides_of_array;
 
 public class ListTool {
     public int findEvenIndexFrom(int[] numbers) {
-        return 0;
+        int indexFoundAt = 0;
+        if (numbers.length == 0) {
+            return 0;
+        }
+        if (numbers[0] != numbers[1]) {
+            indexFoundAt = -1;
+        }
+        return indexFoundAt;
     }
 }
--- src/test/java/unit/equal_sides_of_array/TestFindEqualSidesShould.java
+++ src/test/java/unit/equal_sides_of_array/TestFindEqualSidesShould.java
@@ -6,11 +6,20 @@ import org.junit.Test;
 
 public class TestFindEqualSidesShould {
     @Test
-    public void return_zero_when_the_numbers_list_is_empty() throws Exception {
+    public void retrieve_zero_when_the_numbers_list_is_empty() throws Exception {
         int[] numbers = {};
 
         ListTool listTool = new ListTool();
 
         Assert.assertEquals(0, listTool.findEvenIndexFrom(numbers));
+    }
+
+    @Test
+    public void retrieve_minus_one_when_the_numbers_list_of_size_two_does_not_have_any_even_index() throws Exception {
+        int[] numbers = {1,2};
+
+        ListTool listTool = new ListTool();
+
+        Assert.assertEquals(-1, listTool.findEvenIndexFrom(numbers));
     }
 }
