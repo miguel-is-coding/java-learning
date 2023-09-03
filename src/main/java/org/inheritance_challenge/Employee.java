49030623c0066ae98134ab6d8f57e7a6d70ee74d
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-09-03 09:10:25 +0100

    feat(inheritance-challenge): create employee subclass

--- src/main/java/org/inheritance_challenge/Employee.java
+++ src/main/java/org/inheritance_challenge/Employee.java
@@ -1,2 +1,22 @@
-package org.inheritance_challenge;public class Employee {
+package org.inheritance_challenge;
+
+import java.time.LocalDate;
+
+public class Employee extends Worker{
+    private final long employeeId;
+    private final LocalDate hireDate;
+
+    public Employee(String name, LocalDate birthDate, long employeeId, LocalDate hireDate) {
+        super(name, birthDate);
+        this.employeeId = employeeId;
+        this.hireDate = hireDate;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
 }
--- src/test/java/unit/inheritance_challenge/TestEmployeeShould.java
+++ src/test/java/unit/inheritance_challenge/TestEmployeeShould.java
@@ -1,2 +1,27 @@
-package unit.inheritance_challenge;public class Employee {
+package unit.inheritance_challenge;
+
+import org.inheritance_challenge.Employee;
+import org.inheritance_challenge.Worker;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class TestEmployeeShould {
+    @Test
+    public void have_an_employee_id_and_hire_date_assigned() throws Exception {
+        int employeeId = 1;
+        LocalDate hireDate = LocalDate.of(2022, Month.JULY, 1);
+
+        Employee employee = new Employee(
+                "Maria",
+                LocalDate.of(1994, Month.MARCH, 8),
+                employeeId,
+                hireDate
+        );
+
+        Assert.assertEquals(employeeId, employee.getEmployeeId());
+        Assert.assertTrue(hireDate.isEqual(employee.getHireDate()));
+    }
 }
