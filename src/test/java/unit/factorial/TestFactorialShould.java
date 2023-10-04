0370c8ee6529cd6a7e0321449c12f3f5fb7925b5
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-10-04 06:58:33 +0100

    test(factorial): create red test when given number is 2 and should return 2

--- src/test/java/unit/factorial/TestFactorialShould.java
+++ src/test/java/unit/factorial/TestFactorialShould.java
@@ -20,4 +20,13 @@ public class TestFactorialShould {
 
         Assert.assertEquals(1, numberOperator.factorial(number));
     }
+
+    @Test
+    public void give_2_when_given_number_is_2() {
+        int input = 2;
+
+        NumberOperator numberOperator = new NumberOperator();
+
+        Assert.assertEquals(2, numberOperator.factorial(input));
+    }
 }
