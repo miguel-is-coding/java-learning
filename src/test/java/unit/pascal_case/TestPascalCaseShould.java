e9a7ffdd586d5b48fa80bfe29b70ebf794e4e82b
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-10-04 07:36:14 +0100

    test(pascal-case): create red test for minimal case when text is empty

---
+++ src/test/java/unit/pascal_case/TestPascalCaseShould.java
@@ -0,0 +1,28 @@
+package unit.pascal_case;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPascalCaseShould {
+    /*
+     * Having a text whose words can be delimited by spaces,
+     * hyphens and/or underscores, return the text in pascal case format
+     * (The first letter of each word is capitalized).
+     *
+     * Example
+     * text = ''  // => ''
+     * text = 'Foo'  // => 'Foo'
+     * text = 'Foo Bar'  // => 'FooBar'
+     * text = 'Foo_Bar-Foo' // => 'FooBarFoo'
+     * text = 'foo'  // => 'Foo'
+     * text = 'foo_bar foo-bar' // => 'FooBarFooBar'
+     */
+    @Test
+    public void do_not_change_text_when_text_is_empty() {
+        String text = "";
+
+        TextFormatter textFormatter = new TextFormatter();
+
+        Assert.assertEquals("", textFormatter.pascalCase(text));
+    }
+}
