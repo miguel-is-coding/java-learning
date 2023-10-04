38df82a42bc219e42d92cb82d376fffba3d27bc6
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-10-04 06:56:58 +0100

    feat(factorial): create factorial function to return number 1 when given number is 1

--- src/main/java/org/factorial/NumberOperator.java
+++ src/main/java/org/factorial/NumberOperator.java
@@ -1,2 +1,7 @@
-package org.factorial;public class NumberOperator {
+package org.factorial;
+
+public class NumberOperator {
+    public int factorial(int number) {
+        return 1;
+    }
 }
--- src/test/java/unit/factorial/TestFactorialShould.java
+++ src/test/java/unit/factorial/TestFactorialShould.java
@@ -1,2 +1,23 @@
-package unit.factorial;public class TestFactorialShould {
+package unit.factorial;
+
+import org.factorial.NumberOperator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestFactorialShould {
+//    The factorial function multiplies all the integers and positive
+//    numbers between the number that arrives by parameter and the number 1.
+// 1 => 1
+// 2 => 2
+// 3 => 6
+// 4 => 24
+
+    @Test
+    public void give_1_when_given_number_is_1() {
+        int number = 1;
+
+        NumberOperator numberOperator = new NumberOperator();
+
+        Assert.assertEquals(1, numberOperator.factorial(number));
+    }
 }
